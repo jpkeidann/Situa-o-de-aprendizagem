bcf32a1f3eb68035870a7d146e8dbfafc878168a
jpkeidann <joao_keidann@estudante.sesisenai.org.br>
2025-11-27 12:45:47 -0300

    mudando design AAAAAAA

--- canvas.js
+++ canvas.js
@@ -1,9 +1,13 @@
-const canvas = document.getElementById('canvasInicial');
+const canvas = document.querySelector('canvas')
+
+const local = window.location.pathname
 
 const canvasWidth = window.innerWidth
 const canvasheight = window.innerHeight
 canvas.width = canvasWidth
 canvas.height = canvasheight
+
+console.log(window)
 var ctx = canvas.getContext('2d')
 
 var mouse = {
@@ -11,13 +15,13 @@ var mouse = {
     y: undefined
 }
 
-window.addEventListener('mousemove', (event)=>{
+window.addEventListener('mousemove', (event) => {
     const rect = canvas.getBoundingClientRect();
     mouse.x = event.clientX - rect.left;
     mouse.y = event.clientY - rect.top;
 })
 
-let colorArr = [
+let colorArrIndex = [
     '#5dd0de',
     '#f7f3cf',
     '#27b1bf',
@@ -25,38 +29,48 @@ let colorArr = [
     '#176585'
 ]
 
-function Circle(x,y,dx,dy,radius){
+let colorArr = [
+    '#ffffff'
+]
+
+
+function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
-    this.color = colorArr[Math.floor(Math.random() * colorArr.length)]
+
+    if (local === '/index.html') {
+        this.color = colorArrIndex[Math.floor(Math.random() * colorArrIndex.length)]
+    } else {
+        this.color = colorArr[Math.floor(Math.random() * colorArr.length)]
+    }
 
     this.draw = () => {
         ctx.beginPath();
-        ctx.arc( this.x , this.y , this.radius, 0 , Math.PI * 2 , false)
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         ctx.strokeStyle = this.color
         ctx.fillStyle = this.color
         ctx.fill()
         ctx.stroke()
     }
 
-    this.update = ()=>{
-        if(this.x + this.radius > canvasWidth  || this.x - this.radius < 0 ){
+    this.update = () => {
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
             this.dx = -this.dx;
         }
-        if(this.y + this.radius > canvasheight  || this.y - this.radius < 0 ){
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
             this.dy = -this.dy;
         }
-        
+
         this.x += this.dx
         this.y += this.dy
 
         // interactivity
-        if(mouse.x - this.x < 70 && mouse.x - this.x > -70 && mouse.y - this.y < 70 && mouse.y - this.y > -70 && this.radius < 30){
+        if (mouse.x - this.x < 70 && mouse.x - this.x > -70 && mouse.y - this.y < 70 && mouse.y - this.y > -70 && this.radius < 30) {
             this.radius += 2;
-        }else if(this.radius > 5){
+        } else if (this.radius > 5) {
             this.radius -= 2;
         }
 
@@ -65,19 +79,19 @@ function Circle(x,y,dx,dy,radius){
 }
 
 let circleArr = [];
-for(let i = 0; i < 700; i++){
-    const radius =  Math.random() * 10
-    var x = Math.random() * canvasWidth;
-    var y = Math.random() * canvasheight;
+for (let i = 0; i < 700; i++) {
+    const radius = Math.random() * 10
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
     const dx = (Math.random() - 0.5) * 2
     const dy = (Math.random() - 0.5) * 2
-    circleArr.push(new Circle(x,y,dx,dy,radius))
+    circleArr.push(new Circle(x, y, dx, dy, radius))
 }
 
-function animate(){
+function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0,0, canvasWidth, canvasheight)
-    for(let i = 0; i < circleArr.length; i++){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < circleArr.length; i++) {
         circleArr[i].update();
     }
 }
